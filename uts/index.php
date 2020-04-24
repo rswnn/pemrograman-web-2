a4adce8e447200b05f9b4b51374a08383885cc20
Riswan Ardiansah <riswanardiansah@gmail.com>
2020-04-24 23:27:55 +0700

    menambahkan uts

---
+++ uts/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Riswan Ardiansah 171011401402</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+	<body>
+		<form method="post" action="submit.php">
+			<p class="label">Nama Wilayah </p>
+			<select class="input-style" name="wilayah">
+				<option>Nama Wilayah</option>
+				<option value="DKI Jakarta">DKI Jakarta</option>
+				<option value="Jawa Barat">Jawa Barat</option>
+				<option value="Banten">Banten</option>
+				<option value="Jawa Tengah">Jawa Tengah</option>
+			</select><br>
+			<p class="label">Jumlah Positif</p><input class="input-style" type="text" name="positive"><br>
+			<p class="label">Jumlah Dirawat</p><input class="input-style" type="text" name="dirawat"><br>
+			<p class="label">Jumlah Sembuh</p><input class="input-style" type="text" name="sembuh"><br>
+			<p class="label">Jumlah Meninggal</p> <input class="input-style" type="text" name="meninggal"><br>
+			<p class="label">Nama Operator</p> <input class="input-style" type="text" name="nama"><br>
+			<p class="label">NIM Mahasiswa</p> <input class="input-style" type="text" name="nim"><br>
+			<input type="submit"></input>
+		</form>
+	</body>
+</body>
+
+</html>
---
+++ uts/submit.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riswan Ardiansah 171011401402</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="result">
+        <div class="result-header">
+            <h1>Data Pemantauan COVID19 Wilayah <?php echo $_POST["wilayah"]; ?> <br>
+                <?php $tanggal = new DateTime('now', new DateTimezone('Asia/Jakarta')); ?>
+                Per <?php echo $tanggal->format("d-F-y H:i:s"); ?> <br>
+                <?php echo $_POST['nama']; ?>
+                <?php echo $_POST['nim']; ?>
+        </div>
+        <table>
+            <tr>
+                <th>Positif</th>
+                <th>Dirawat</th>
+                <th>Sembuh</th>
+                <th>Meninggal</th>
+            </tr>
+            <tr>
+                <th><?php echo $_POST["positive"]; ?></th>
+                <th><?php echo $_POST["dirawat"]; ?></th>
+                <th><?php echo $_POST["dirawat"]; ?></th>
+                <th><?php echo $_POST["meninggal"]; ?></th>
+            </tr>
+        </table>
+        </h1>
+    </div>
+    <?php
+    $wilayah = $_POST["wilayah"];
+    $positive = $_POST["positive"];
+    $dirawat = $_POST["dirawat"];
+    $dirawat = $_POST["dirawat"];
+    $meninggal = $_POST["meninggal"];
+    $nama = $_POST["nama"];
+    $nim = $_POST["nim"];
+    $fp = fopen('data.txt', 'w');
+    fwrite($fp, $wilayah);
+    fwrite($fp, $positive);
+    fwrite($fp, $dirawat);
+    fwrite($fp, $dirawat);
+    fwrite($fp, $meninggal);
+    fwrite($fp, $nama);
+    fwrite($fp, $nim);
+    fclose($fp);
+    ?>
+</body>
+
+</html>
